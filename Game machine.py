7126b4a52d9de541ad4a52ba2b6c1448197fa797
PiotrPolaczek <132391334+PiotrPolaczek@users.noreply.github.com>
2023-06-20 02:47:18 +0200

    Add files via upload

---
+++ Game machine.py
@@ -0,0 +1,41 @@
+import random
+a=0
+b=9
+digit1 = None
+digit2 = None
+digit3 = None
+digit4 = None
+digit5 = None
+digit6 = None
+digit7 = None
+digit8 = None
+digit9 = None
+digit10 = None
+digit11 = None
+digit12 = None
+digit13 = None
+digit14 = None
+digit15 = None
+digit16 = None
+
+
+
+digit1 = random.randint(a, b)
+digit2 = random.randint(a, b)
+digit3 = random.randint(a, b)
+digit4 = random.randint(a, b)
+digit5 = random.randint(a, b)
+digit6 = random.randint(a, b)
+digit7 = random.randint(a, b)
+digit8 = random.randint(a, b)
+digit9 = random.randint(a, b)
+digit10 = random.randint(a, b)
+digit11 = random.randint(a, b)
+digit12 = random.randint(a, b)
+digit13 = random.randint(a, b)
+digit14 = random.randint(a, b)
+digit15 = random.randint(a, b)
+digit16 = random.randint(a, b)
+
+
+print(digit1,digit2,digit3,digit4,digit5,digit6,digit7,digit8,digit9,digit10,digit11,digit12,digit13,digit14,digit15,digit16)
